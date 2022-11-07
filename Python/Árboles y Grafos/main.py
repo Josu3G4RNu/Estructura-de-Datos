39b1c8752d9166f714dd3308f05bbf818c65b71e
Josu3G4RNu <josuegarnu205@gmail.com>
2022-11-06 21:04:28 -0600

    Se finaliza el arranque del programa

--- Python/Árboles y Grafos/main.py
+++ Python/Árboles y Grafos/main.py
@@ -1,5 +1,7 @@
 from Arboles_Binarios import Arboles_Binarios, recorrer_arbol
+from Grafos import Grafo
 
+# Arboles
 # Creación de un objeto del tipo arbol binario
 prueba = Arboles_Binarios()
 
@@ -17,5 +19,39 @@ prueba.add_node(5)
 # Recorremos e imprimimos el árbol
 recorrer_arbol(prueba.raiz)
 
-# Imprimimos la cantidad e nodos hihos dentro del árbol
+# Imprimimos la cantidad e nodos hijos dentro del árbol
 print(prueba.count_sheets(prueba.raiz))
+
+
+# Grafos
+# Creamos un objeto de tipo Grafo
+grafo = Grafo()
+
+# Añadimos los vertices al grafo
+grafo.add_vertice('A')
+grafo.add_vertice('B')
+grafo.add_vertice('C')
+grafo.add_vertice('D')
+grafo.add_vertice('E')
+grafo.add_vertice('F')
+grafo.add_vertice('G')
+grafo.add_vertice('H')
+
+# Añadimos los ejes correspondientes
+grafo.add_arista('A', 'D', 7)
+grafo.add_arista('A', 'G', 16)
+grafo.add_arista('B', 'F', 21)
+grafo.add_arista('C', 'B', 16)
+grafo.add_arista('C', 'E', 12)
+grafo.add_arista('C', 'H', 9)
+grafo.add_arista('D', 'B', 8)
+grafo.add_arista('E', 'D', 9)
+grafo.add_arista('E', 'F', 6)
+grafo.add_arista('F', 'G', 8)
+grafo.add_arista('G', 'B', 13)
+grafo.add_arista('G', 'C', 12)
+grafo.add_arista('H', 'A', 8)
+grafo.add_arista('H', 'D', 17)
+
+print(grafo.dijkstra('B'))
+grafo.imprimir_grafo()
